273459671b4f528a54168807c61dadec744b4f9d
Yunus Bayu Aji <yunus.bayu.aji@mail.ugm.ac.id>
2021-03-13 21:23:35 +0700

    update pick place

--- catkin_ws/src/paket_ayam/scripts/ayam_vision.py
+++ catkin_ws/src/paket_ayam/scripts/ayam_vision.py
@@ -33,6 +33,7 @@ yI = 0.0
 yD = 0
 check = 0
 yaw_memory = 90
+kelas = 1
 
 lock_target = False
 
@@ -65,7 +66,10 @@ rate = rospy.Rate(60)
 try:
     serialArduino = serial.Serial(port = "/dev/ttyUSB0", baudrate = 9600)
 except:
-    print("No Serial")
+    while not serialArduino.isOpen():
+        print("Wait Serial Opened")
+        serialArduino = serial.Serial(port = "/dev/ttyUSB0", baudrate = 9600)
+        time.sleep(1)
 
 def gstreamer_pipeline(
     capture_width=300,
@@ -96,7 +100,7 @@ def gstreamer_pipeline(
 
 
 def show_camera():
-    global yP, yI, yD, check, yaw_memory, lock_target
+    global yP, yI, yD, check, yaw_memory, lock_target, kelas
     # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
     print(gstreamer_pipeline(flip_method=0))
     cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
@@ -122,126 +126,134 @@ def show_camera():
     with detection_graph.as_default():
         with tf.Session(graph=detection_graph) as sess:
             while True:
-                # for x in os.listdir():
-                fps = time.time()
-                # Read frame from camera
-                ret, image_np = cap.read()
-                # image_np = cv2.resize(image_np, (50, 50), interpolation = cv2.INTER_AREA)
-            
-                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-                image_np_expanded = np.expand_dims(image_np, axis=0)
-                # Extract image tensor
-                image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-                # Extract detection boxes
-                boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-                # Extract detection scores
-                scores = detection_graph.get_tensor_by_name('detection_scores:0')
-                # Extract detection classes
-                classes = detection_graph.get_tensor_by_name('detection_classes:0')
-                # Extract number of detections
-                num_detections = detection_graph.get_tensor_by_name(
-                    'num_detections:0')
-                # Actual detection.
-                (boxes, scores, classes, num_detections) = sess.run(
-                    [boxes, scores, classes, num_detections],
-                    feed_dict={image_tensor: image_np_expanded})
+                try:
+                    # for x in os.listdir():
+                    fps = time.time()
+                    # Read frame from camera
+                    ret, image_np = cap.read()
+                    # image_np = cv2.resize(image_np, (50, 50), interpolation = cv2.INTER_AREA)
                 
-                _index = np.argmax(np.squeeze(scores))
-                score = np.squeeze(scores)[_index]
-                x1 = np.asarray(300 * np.squeeze(boxes)[_index])[1]
-                y1 = np.asarray(300 * np.squeeze(boxes)[_index])[0]
-                x2 = np.asarray(300 * np.squeeze(boxes)[_index])[3]
-                y2 = np.asarray(300 * np.squeeze(boxes)[_index])[2]
-                cat = np.squeeze(classes)[_index]
-
-                if score>0.6:
+                    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                    image_np_expanded = np.expand_dims(image_np, axis=0)
+                    # Extract image tensor
+                    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+                    # Extract detection boxes
+                    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+                    # Extract detection scores
+                    scores = detection_graph.get_tensor_by_name('detection_scores:0')
+                    # Extract detection classes
+                    classes = detection_graph.get_tensor_by_name('detection_classes:0')
+                    # Extract number of detections
+                    num_detections = detection_graph.get_tensor_by_name(
+                        'num_detections:0')
+                    # Actual detection.
+                    (boxes, scores, classes, num_detections) = sess.run(
+                        [boxes, scores, classes, num_detections],
+                        feed_dict={image_tensor: image_np_expanded})
                     
+                    _index = np.argmax(np.squeeze(scores))
+                    score = np.squeeze(scores)[_index]
+                    x1 = np.asarray(300 * np.squeeze(boxes)[_index])[1]
+                    y1 = np.asarray(300 * np.squeeze(boxes)[_index])[0]
+                    x2 = np.asarray(300 * np.squeeze(boxes)[_index])[3]
+                    y2 = np.asarray(300 * np.squeeze(boxes)[_index])[2]
+                    cat = np.squeeze(classes)[_index]
 
-                    cv2.rectangle(image_np, (x1,y1), (x2,y2), (0,255,0), 2)
-                    cv2.circle(image_np, (int((x1+x2)/2), int((y1+y2)/2)), 5, (0,255,0), 2)
+                    if score>0.6:
+                        kelas = int(cat)
+                        
 
-                    target_X = int((x1+x2)/2)
-                    yaw_pid = PID(yP, yI, yD, setpoint=int(frameWidth/2), output_limits=(-0.75,0.75))
-                    yaw_axis = yaw_pid(target_X)
-                    error = math.sqrt(math.pow((target_X-int(frameWidth/2)),2))
+                        cv2.rectangle(image_np, (x1,y1), (x2,y2), (0,255,0), 2)
+                        cv2.circle(image_np, (int((x1+x2)/2), int((y1+y2)/2)), 5, (0,255,0), 2)
 
-                    if error<20 :
-                        lock_target = True
-                        yaw_memory = yaw_axis
-                    else:
-                        dataWrite = "{}{},{},{},{},{},{}{}{}".format("*", 90, 0, 30, 90, yaw_axis, 0, "#","\n")
-                    
-                    if lock_target and check >50:
-                        if error<20:
-                            dataWrite = "{}{},{},{},{},{},{}{}{}".format("*", 0, 0, 30, 90, int(frameWidth/2), 1, "#","\n")
+                        target_X = int((x1+x2)/2)
+                        yaw_pid = PID(yP, yI, yD, setpoint=int(frameWidth/2), output_limits=(-0.75,0.75))
+                        yaw_axis = yaw_pid(target_X)
+                        error = math.sqrt(math.pow((target_X-int(frameWidth/2)),2))
+
+                        if error<20 :
+                            lock_target = True
+                            yaw_memory = yaw_axis
                         else:
-                            lock_target = False
-                            check = 0
+                            dataWrite = "{}{},{},{},{},{},{},{},{}{}{}".format("*", 90, 0, 30, 85, yaw_axis, 0, 0, kelas, "#","\n")
+                        
+                        if lock_target and check >50:
+                            if error<20:
+                                dataWrite = "{}{},{},{},{},{},{},{},{}{}{}".format("*", 0, 0, 30, 85, 0, 1, 0, kelas, "#","\n")
+                            else:
+                                lock_target = False
+                                check = 0
+                        else:
+                            check += 1
+                            yaw_memory = yaw_axis
+                            dataWrite = "{}{},{},{},{},{},{},{},{}{}{}".format("*", 90, 0, 30, 85, yaw_memory, 0, 0, kelas, "#","\n")
+                        print(check)
+                        print(dataWrite)
+                        try:
+                            serialArduino.write(dataWrite.encode())
+                        except:
+                            print("No Serial")
+                        
+                        gettargetData = str(target_X)
+                        geterrorData = str(error)
+                        getdetectData = str(cat)
                     else:
-                        check += 1
-                        yaw_memory = yaw_axis
-                        dataWrite = "{}{},{},{},{},{},{}{}{}".format("*", 90, 0, 30, 90, yaw_memory, 0, "#","\n")
-                    print(check)
-                    print(dataWrite)
-                    try:
-                        serialArduino.write(dataWrite.encode())
-                    except:
-                        print("No Serial")
+                        dataWrite = "{}{},{},{},{},{},{},{},{}{}{}".format("*", 90, 0, 30, 85, 0, 0, 0, kelas, "#","\n")  
+                        try:
+                            serialArduino.write(dataWrite.encode())
+                        except:
+                            try:
+                                serialArduino = serial.Serial(port = "/dev/ttyUSB0", baudrate = 9600)
+                            except:
+                                print("Plug STM32 Please")
+                            print("No Serial")
+                        
+                        gettargetData = str(gettargetData)
+                        geterrorData = "NULL"
+                        getdetectData = "NULL"
+
+                        print("None of Object Detected")
+
+                    # Visualization of the results of a detection.
+                    # vis_util.visualize_boxes_and_labels_on_image_array(
+                    #     image_np,
+                    #     np.squeeze(boxes),
+                    #     np.squeeze(classes).astype(np.int32),
+                    #     np.squeeze(scores),
+                    #     category_index,
+                    #     use_normalized_coordinates=True,
+                    #     line_thickness=3
+                    # )
+
+                    # Display output
+                    cv2.imshow('Gun Detection', cv2.resize(image_np, (640, 480)))
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        cv2.destroyAllWindows()
+                        break
+                    print(1/(time.time()-fps))
+                    # time.sleep(0.5)
+                    # cv2.waitKey(0)
+                    # break
+
+                    rate.sleep()
+
+                    waktu += time.time()-fps
+
+                    #Data Published
+                    targetData.data = gettargetData
+                    errorData.data = geterrorData
+                    detectData.data = getdetectData
+                    timeData.data = "%1.2f" % (waktu)
+                    imageData.format = "JPEG"
+                    imageData.data = np.array(cv2.imencode('.jpeg', image_np)[1]).tostring()
                     
-                    gettargetData = str(target_X)
-                    geterrorData = str(error)
-                    getdetectData = str(cat)
-                else:
-                    dataWrite = "{}{},{},{},{},{},{}{}{}".format("*", 90, 0, 30, 90, 0, 0, "#","\n")  
-                    try:
-                        serialArduino.write(dataWrite.encode())
-                    except:
-                        print("No Serial")
-                    
-                    gettargetData = str(gettargetData)
-                    geterrorData = "NULL"
-                    getdetectData = "NULL"
-
-                    print("None of Object Detected")
-
-                # Visualization of the results of a detection.
-                # vis_util.visualize_boxes_and_labels_on_image_array(
-                #     image_np,
-                #     np.squeeze(boxes),
-                #     np.squeeze(classes).astype(np.int32),
-                #     np.squeeze(scores),
-                #     category_index,
-                #     use_normalized_coordinates=True,
-                #     line_thickness=3
-                # )
-
-                # Display output
-                cv2.imshow('Gun Detection', cv2.resize(image_np, (640, 480)))
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    cv2.destroyAllWindows()
-                    break
-                print(1/(time.time()-fps))
-                # time.sleep(0.5)
-                # cv2.waitKey(0)
-                # break
-
-                rate.sleep()
-
-                waktu += time.time()-fps
-
-                #Data Published
-                targetData.data = gettargetData
-                errorData.data = geterrorData
-                detectData.data = getdetectData
-                timeData.data = "%1.2f" % (waktu)
-                imageData.format = "JPEG"
-                imageData.data = np.array(cv2.imencode('.jpeg', image_np)[1]).tostring()
-                
-                targetPub.publish(targetData)
-                errorPub.publish(errorData)
-                detectPub.publish(detectData)
-                timePub.publish(timeData)
-                imagePub.publish(imageData)
+                    targetPub.publish(targetData)
+                    errorPub.publish(errorData)
+                    detectPub.publish(detectData)
+                    timePub.publish(timeData)
+                    imagePub.publish(imageData)
+                except Exception as e:
+                    print(e)
 
 if __name__ == "__main__":
     show_camera()
